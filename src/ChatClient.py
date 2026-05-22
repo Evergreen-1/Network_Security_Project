a6210119091a85fe35b11f316357076e4c068d86
ruairí <ruairimkmail@gmail.com>
2026-05-22 17:12:38 +0200

    Update files

--- src/ChatClient.py
+++ src/ChatClient.py
@@ -1,51 +1,108 @@
 # KRNRUA001
 
+import ChatProtocol
+from eventDataclasses import UsernameChangeEvent, ServerShutdownEvent
+import asyncio
+
 class ChatClient:
 
-    def __init__(protocol: ChatProtocol):
+    def __init__(self, chat_protocol: ChatProtocol):
+        self.chat_protocol = chat_protocol
+        self.session = None
+        self.username = None
+        self.channels = set()
+        self.on_event = None
+        self.ping_task = None
+        self.event_task = None
+
+    async def connect(self):
+        await self.chat_protocol.start()
+        response = await self.chat_protocol.send({"request_type": 1})
+        
+        self.session = response.get("session")
+        self.chat_protocol.session = self.session
+        self.username = response.get("username")
+
+        self.ping_task = asyncio.create_task(self.ping())
+        self.event_task = asyncio.create_task(self.events())
+
+        return response.get("message")
+
+    async def ping(self):
+        while True:
+            try:
+                await asyncio.sleep(30)
+                await self.chat_protocol.send({"request_type": 3})
+            except asyncio.CancelledError:
+                break
+
+    async def disconnect(self):
+        if self.ping_task and not self.ping_task.done():
+            self.ping_task.cancel()
+        if self.event_task and not self.event_task.done():
+            self.event_task.cancel()
+
+        try:
+            response = await asyncio.shield(self.chat_protocol.send({"request_type": 2}))
+            return response.get("message")
+        except Exception as e:
+            return "failed to deliver disconnect message: {e}"
+        finally:
+            self.session = None
+            self.chat_protocol.session = None
+            self.username = None
+            self.channels.clear()
+
+    async def create_channel(self, channel, description):
         pass
 
-    def connect(host, port):
+    async def list_channels(self):
         pass
 
-    def ping():
+    async def channel_info(self, channel):
         pass
 
-    def disconnect():
+    async def join_channel(self, channel):
         pass
 
-    def create_channel(channel, description):
+    async def leave_channel(self, channel):
         pass
 
-    def list_channels():
+    async def message_channel(self, channel, message):
         pass
 
-    def channel_info(channel):
+    async def set_username(self, username):
         pass
 
-    def join_channel(channel):
+    async def list_users(self, channel=None):
         pass
 
-    def leave_channel(channel):
+    async def message_user(self, username, message):
         pass
 
-    def message_channel(channel, message):
+    async def whoami(self):
         pass
 
-    def set_username(username):
+    async def whois(self, username):
         pass
 
-    def list_users(channel=None):
-        pass
+    async def events(self):
+        while True:
+            try:
+                event = await self.chat_protocol.event_queue.get()
 
-    def message_user(username, message):
-        pass
+                if isinstance(event, UsernameChangeEvent):
+                    if event.old_username == self.username:
+                        self.username = event.new_username
 
-    def whoami():
-        pass
+                if isinstance(event, ServerShutdownEvent):
+                    self.session = None
+                    self.chat_protocol.session = self.session
+                    self.username = None
+                    self.channels.clear()
 
-    def whois(username):
-        pass
+                if self.on_event:
+                    self.on_event(event)
 
-    def events():
-        pass
+            except asyncio.CancelledError:
+                break
--- src/ChatProtocol.py
+++ src/ChatProtocol.py
@@ -3,6 +3,16 @@
 import asyncio
 import msgpack
 import random
+from eventDataclasses import (
+    UserMessageEvent,
+    ChannelMessageEvent,
+    ChannelJoinEvent,
+    ChannelLeaveEvent,
+    UsernameChangeEvent,
+    ServerMessageEvent,
+    ServerShutdownEvent
+)
+from networkProtocols import BaseProtocol
 
 class ChatProtocol:
 
@@ -19,12 +29,15 @@ class ChatProtocol:
         try:
             return msgpack.unpackb(incoming_data, raw=False)
         except msgpack.UnpackException:
-            raise RuntimeError("unpack was unsuccessful")
+            raise RuntimeError("unpack was unsuccessful") from None
+        
+    async def start(self):
+        await self.transport_protocol.start(self.on_response)
 
     async def send(self, outgoing_data: dict) -> dict:
         if self.session:
             outgoing_data["session"] = self.session
-        elif outgoing_data["request_type"] is not 1:
+        elif outgoing_data["request_type"] != 1:
             raise RuntimeError("not in a session")
         
         while True:
@@ -55,6 +68,10 @@ class ChatProtocol:
         if not isinstance(response, dict):
             print("response isnt a dictionary")
             return
+        
+        if "response_type" not in response or "response_handle" not in response:
+            print("response doesnt have expected fields for some reason")
+            return
 
         response_type = response.get("response_type")
         response_handle = response.get("response_handle")
@@ -62,14 +79,54 @@ class ChatProtocol:
         if response_handle is not None and response_handle in self.pending: # reply
             target_future = self.pending[response_handle]
             if not target_future.done():
-                if response_type is 20 or "error" in response:
+                if response_type == 20 or "error" in response:
                     target_future.set_exception(RuntimeError(response.get("error")))
                 else:
                     target_future.set_result(response)
         else:   # unsolicited response
-            event = self.make_event(response) # TODO: MAYBE ADD TRY CATCH??
-            if event:
-                 self.event_queue.put_nowait(event)
+            try:
+                event = self.make_event(response_type, response)
+                if event:
+                    self.event_queue.put_nowait(event)
+            except Exception as e:
+                print(f"issue with unsolicited response: {e}")
 
-    def make_event(self, response: dict):
-         pass
+    def make_event(self, response_type, response: dict):
+        match response_type:
+            case 33:    # User Message
+                return UserMessageEvent(
+                    username=response["from_username"],
+                    message=response["message"]
+                )
+            case 30:    # Channel Message
+                return ChannelMessageEvent(
+                    channel=response["channel"],
+                    username=response["username"],
+                    message=response["message"]
+                )
+            case 28:    # Channel Join
+                return ChannelJoinEvent(
+                    channel=response["channel"],
+                    username=response["username"],
+                    description=response["description"]
+                )
+            case 29:    # Channel Leave
+                return ChannelLeaveEvent(
+                    channel=response["channel"],
+                    username=response["username"]
+                )
+            case 34:    # Username Change
+                return UsernameChangeEvent(
+                    old_username=response["old_username"],
+                    new_username=response["new_username"]
+                )
+            case 36:    # Server Message
+                return ServerMessageEvent(
+                    message=response["message"]
+                )
+            case 37:    # Server Shutdown
+                return ServerShutdownEvent(
+                    message=response["message"]
+                )
+            case _:
+                return None
