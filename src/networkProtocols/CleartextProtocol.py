bf3aad57f4fea32de04a23dd6cb20e0e004643c3
ruairí <ruairimkmail@gmail.com>
2026-05-22 17:13:44 +0200

    Update files and add package __init__ file

--- src/networkProtocols/CleartextProtocol.py
+++ src/networkProtocols/CleartextProtocol.py
@@ -1,7 +1,7 @@
 # KRNRUA001
 
+from .BaseProtocol import BaseProtocol
 import asyncio
-import socket
 
 class CleartextProtocol(BaseProtocol, asyncio.DatagramProtocol):
 
@@ -15,12 +15,7 @@ class CleartextProtocol(BaseProtocol, asyncio.DatagramProtocol):
     async def start(self, on_response):
         self.on_response = on_response
         loop = asyncio.get_running_loop()
-
         await loop.create_datagram_endpoint(lambda: self, remote_addr=(self.hostname, self.port))
-
-        # self.sock = asyncio.get_event_loop().create_datagram_endpoint(asyncio.DatagramProtocol, remote_addr=(self.hostname, self.port))
-
-        # self.receive_task = asyncio.create_task(self.receive)
 
     def send(self, data: bytes):
         if self.transport:
@@ -43,43 +38,5 @@ class CleartextProtocol(BaseProtocol, asyncio.DatagramProtocol):
 
     def error_received(self, exception):
         print(f"error: {exception}")
-
-    # def __init__(self):
-    #     self.hostname = "csc4026z.link"
-    #     self.port = 51825
-    #     self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #     self.sock.settimeout(2.0)
-    #     self._receive = None
-
-    # def start(self, on_receive):
-    #     self._receive = on_receive
-
-    # def send(self, data: bytes):
-    #     max_retries = 3
-    #     num_attempts = 0
-
-    #     while num_attempts < max_retries:
-    #         try:
-    #             print(f"sending request: {num_attempts+1}/{max_retries}")
-    #             self.sock.sendto(data, (self.hostname, self.port))
-
-    #             response_data, server_address = self.sock.recvfrom(2048)
-    #             print(f"response received from {server_address}")
-
-    #             if self._receive:
-    #                 self._receive(response_data)
-
-    #             return
-        
-    #         except socket.timeout:
-    #             num_attempts += 1
-    #             print("request timed out on attempt {num_attempts}")
-    #             if num_attempts >= max_retries:
-    #                 print("max retries attempted")
-
-    #         except socket.gaierror:
-    #             print("dns resolution failed")
-    #             break
-
-    # def close(self):
-    #     self.sock.close()
+        if self.transport:
+            self.transport.close()
